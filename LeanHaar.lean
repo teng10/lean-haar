2e7f37b463237b72df444f3651c0e4698ca0ae70
Melody Lee <melody.lee357@gmail.com>
2026-06-18 13:36:05 +0200

    Merge branch 'main' of https://github.com/teng10/lean-haar
    No overlapping files, but need to merge similar changes

--- LeanHaar.lean
+++ LeanHaar.lean
@@ -1,5 +1,6 @@
 import LeanHaar.HilbertSpace
 import LeanHaar.TensorPower
+import LeanHaar.UnitaryPower
 import LeanHaar.Permutation
 import LeanHaar.Example
 import LeanHaar.Examples.TwirlingPhyslibV2

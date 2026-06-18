8e3ac02a014298749c7e0fdc7b8579c2284cf467
Melody Lee <melody.lee357@gmail.com>
2026-06-18 13:43:55 +0200

    Fix import of file I have not committed yet.

--- LeanHaar.lean
+++ LeanHaar.lean
@@ -1,6 +1,5 @@
 import LeanHaar.HilbertSpace
 import LeanHaar.TensorPower
-import LeanHaar.UnitaryPower
 import LeanHaar.Permutation
 import LeanHaar.Example
 import LeanHaar.Examples.TwirlingPhyslibV2

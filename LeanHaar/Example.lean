d2109afb3c880c94c36e5e421be18a4b938e26c4
Yanting Teng <adateng07@gmail.com>
2026-07-10 09:26:36 +0200

    basic example of matrix equalitz

--- LeanHaar/Example.lean
+++ LeanHaar/Example.lean
@@ -11,3 +11,11 @@ def basicAdd (x y : Nat) : Nat :=
   x + y
 
 #eval basicAdd 101 202 -- 303
+
+-- the identity matrix, named two equivalent ways
+#check (1 : Matrix (Fin 2) (Fin 2) Nat)
+
+-- it literally is ![![1,0],![0,1]]
+example : (1 : Matrix (Fin 2) (Fin 2) Nat) = ![![1, 0], ![0, 1]] := by
+  ext i j
+  fin_cases i <;> fin_cases j <;> simp
